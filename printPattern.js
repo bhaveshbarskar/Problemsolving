305c35afebe3aa038e71c73f1c23a627292c5ef7
Bhavesh barskar <barskar.bhavesh@gmail.com>
2019-04-30 04:17:45 +0530

    to print right shifted right angled triangle using js

--- printPattern.js
+++ printPattern.js
@@ -1,16 +1,13 @@
 var a = () => {
-  var n = 4;
-  var space = n - 1;
+  var n = 6;
 
-  for (let i = 0; i < n; i++) {
-    for (let j = n; j > i; j--) console.log("A");
+  for (let i = 1; i <= n; i++) {
+    for (let j = n - 1; j >= i; j--) {
+      process.stdout.write(" ");
+    }
+
+    for (let z = 1; z <= i; z++) process.stdout.write("#");
+    process.stdout.write("\n");
   }
 };
 a();
-
-
-git init
-git add README.md
-git commit - m "first commit"
-git remote add origin https://github.com/bhaveshbarskar/Problemsolving.git
-git push - u origin master
